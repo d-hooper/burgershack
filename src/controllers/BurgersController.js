afbe8f4a3191bf822506070e9d9d72109ecedbb9
d-hooper <dallinhoop3@gmail.com>
2025-03-10 14:49:30 -0600

    test post response on postman

--- src/controllers/BurgersController.js
+++ src/controllers/BurgersController.js
@@ -7,12 +7,22 @@ export class BurgersController extends BaseController{
     super('api/burgers');
     this.router
       .get('', this.getAllBurgers)
+      .post('', this.postBurger)
   }
   
   async getAllBurgers(request, response, next) {
     try {
       const burgers = await burgersService.getAllBurgers()
       response.send(burgers)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async postBurger(request, response, next) {
+    response.send('posting 🍔')
+    try {
+      
     } catch (error) {
       next(error)
     }
